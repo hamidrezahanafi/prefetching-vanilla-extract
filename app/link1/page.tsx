bf2594ac25cbf74c90e225abdcd9ce9b369c17d3
Hamidreza Hanafi <hamidreza.hanafi@faire.com>
2025-01-14 17:21:20 -0500

    add timeout

--- app/link1/page.tsx
+++ app/link1/page.tsx
@@ -2,7 +2,7 @@ import { content } from "./style.css";
 import { Box } from './box';
 import { Suspense } from "react";
 export default async function Page() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <div className={content}>
       <h1>Page 1</h1>
